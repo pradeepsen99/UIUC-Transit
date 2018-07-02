5286f114ea99d7aa2671a0078650ff685a47d415
pradeepkumar <pradeepsen99@gmail.com>
2018-07-02 21:12:41 +0530

    Merged old project files with new project files

---
+++ mtd-bus-app-iOS/StopBusListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  StopBusListViewController.swift
+//  mtd-bus-app-iOS
+//
+//  Created by Pradeep Kumar on 7/2/18.
+//  Copyright © 2018 Pradeep Kumar. All rights reserved.
+//
+
+import UIKit
+
+class StopBusListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var currentStop:String = ""
+    
+    init(stop: String) {
+        self.currentStop = stop
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return -1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        return cell
+    }
+    
+    
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor.darkGray
+        //navigationController?
+        print("Worked")
+        print(ViewController().getCurrentStop())
+        if(ViewController().getCurrentStop() == ""){
+            print("fail")
+        }
+        print(currentStop)
+        self.title = currentStop
+    }
+}
+
+extension StopBusListViewController{
+    
+}
--- mtd-bus-app-iOS/ViewController.swift
+++ mtd-bus-app-iOS/ViewController.swift
@@ -2,24 +2,211 @@
 //  ViewController.swift
 //  mtd-bus-app-iOS
 //
-//  Created by Pradeep Kumar on 7/2/18.
+//  Created by Pradeep Kumar on 6/19/18.
 //  Copyright © 2018 Pradeep Kumar. All rights reserved.
 //
 
 import UIKit
+import CoreLocation
+import CoreData
 
-class ViewController: UIViewController {
 
+struct mtd_stop_loc: Codable{
+    let stops: [STOP_INFO]
+    
+    struct STOP_INFO: Codable{
+        let stop_id: String
+        let stop_name: String
+        let code: String
+        let distance: Double
+        let stop_points: [STOP_POINTS]
+        struct STOP_POINTS: Codable{
+            let code: String
+            let stop_id: String
+            let stop_lat: Double
+            let stop_lon: Double
+            let stop_name: String
+        }
+        
+    }
+    
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    //let busListViewCont = StopBusListViewController()
+    
+    var currentStop: String = ""
+    var currentStopCode: String = ""
+    
+    fileprivate var lat: Double = 0
+    fileprivate var long: Double = 0
+    
+    fileprivate var stopNameArr: NSArray = []
+    fileprivate var stopTableView: UITableView!
+    
+    fileprivate var currentStopData: mtd_stop_loc? = nil
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Nearby Stops"
+        
+        view.backgroundColor = UIColor.darkGray
+                
+        // For use when the app is open
+        locationManager.requestWhenInUseAuthorization()
+        // If location services is enabled get the users location
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self as? CLLocationManagerDelegate
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        checkCacheStopData()
+        
+        print("test")
+        
     }
+    
+    func checkCacheStopData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "StopData")
+        request.returnsObjectsAsFaults = false
+        //Fetching data from CoreData
+        do {
+            let result = try context.fetch(request)
+            print(result.count)
+            if(result.count == 0){
+                print("no cache")
+                downloadCurrentStopData()
+            }else{
+                print("cache exists")
+                for data in result as! [NSManagedObject] {
+                    let tempTest: mtd_stop_loc = data.value(forKey: "currentStopList") as! mtd_stop_loc
+                    for i in 0..<tempTest.stops.count {
+                        self.stopNameArr = self.stopNameArr.adding(tempTest.stops[i].stop_name) as NSArray
+                    }
+                }
+                self.displayTable()
+            }
+        } catch {
+            print("Failed")
+        }
+    }
+    
+    
+    func displayTable(){
+        let barHeight: CGFloat = 0
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        self.stopTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        self.stopTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        self.stopTableView.dataSource = self
+        self.stopTableView.delegate = self
+        self.stopTableView.separatorStyle = .none
+        view.addSubview(self.stopTableView)
+    }
+    
+    
+    
+    @IBAction func downloadCurrentStopData (){
+        lat = (locationManager.location?.coordinate.latitude)!
+        long = (locationManager.location?.coordinate.longitude)!
+        guard let gitUrl = URL(string: "https://developer.cumtd.com/api/v2.2/JSON/getstopsbylatlon?key=f298fa4670de47f68a5630304e66227d&lat="+lat.description + "&lon=" + long.description) else { return }
+        
+        URLSession.shared.dataTask(with: gitUrl) { (data, response
+            , error) in
+            
+            guard let data = data else { return }
+            do {
+                let decoder = JSONDecoder()
+                let mtdData = try decoder.decode(mtd_stop_loc.self, from: data)
+                self.currentStopData = mtdData
+                DispatchQueue.main.async {
+                    
+                    for i in 0..<mtdData.stops.count {
+                        self.stopNameArr = self.stopNameArr.adding(mtdData.stops[i].stop_name) as NSArray
+                    }
 
+                    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                    let context = appDelegate.persistentContainer.viewContext
+                    let entity = NSEntityDescription.entity(forEntityName: "StopData", in: context)
+                    let newUser = NSManagedObject(entity: entity!, insertInto: context)
+                    //Adds the stop data to the currentStopList
+                    newUser.setValue(mtdData, forKey: "currentStopList")
+
+                    self.displayTable()
+                }
+            } catch let err {
+                print("Error Downloading data", err)
+            }
+            }.resume()
+    }
+    
+    // If we have been deined access give the user the option to change it
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if(status == CLAuthorizationStatus.denied) {
+            showLocationDisabledPopUp()
+        }
+    }
+    
+    // Show the popup to the user if we have been deined access
+    func showLocationDisabledPopUp() {
+        let alertController = UIAlertController(title: "Background Location Access Disabled",
+                                                message: "In order to get information about your closest stops we need your location",
+                                                preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let openAction = UIAlertAction(title: "Open Settings", style: .default) { (action) in
+            if let url = URL(string: UIApplicationOpenSettingsURLString) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        alertController.addAction(openAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func getCurrentStop()->String{
+        return currentStop
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        currentStop = stopNameArr[indexPath.item] as! String
+        stopBusView()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stopNameArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        cell.textLabel!.text = "\(stopNameArr[indexPath.row])"
+        return cell
+    }
 
 }
+
+extension ViewController {
+    func stopBusView(){
+        navigationController?.pushViewController(StopBusListViewController(stop: currentStop), animated: true)
+        
+    }
+}
+
+
+
+
 
