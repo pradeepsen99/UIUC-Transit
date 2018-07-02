34265777474576844d97ed2a1e0596d02a6547f4
pradeepkumar <pradeepsen99@gmail.com>
2018-07-02 21:30:59 +0530

    RECREATED: WatchKit Project Try #2

---
+++ mtd-bus-app-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mtd-bus-app-iOS
+//
+//  Created by Pradeep Kumar on 7/2/18.
+//  Copyright © 2018 Pradeep Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
